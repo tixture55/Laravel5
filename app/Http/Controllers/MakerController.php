7cdf77c9b789cf78ac3048b4e9085498493de5ae
David Simon <davidthesimon@gmail.com>
2015-03-31 13:26:30 +0100

    Implementing stateless auth
    - Using our own Middleware and an array as SESSION_DRIVER

--- app/Http/Controllers/MakerController.php
+++ app/Http/Controllers/MakerController.php
@@ -12,7 +12,7 @@ class MakerController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth.basic', ['except' => ['index', 'show']]);
+        $this->middleware('auth.basic.once', ['except' => ['index', 'show']]);
     }
 
     /**
--- app/Http/Controllers/MakerVehiclesController.php
+++ app/Http/Controllers/MakerVehiclesController.php
@@ -12,7 +12,7 @@ class MakerVehiclesController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth.basic', ['except' => ['index', 'show']]);
+        $this->middleware('auth.basic.once', ['except' => ['index', 'show']]);
     }
 
     /**
